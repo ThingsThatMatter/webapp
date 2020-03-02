3e35ea63388faf512833b240821114264b6d204d
Tristan Baron <tristan.baron@gmail.com>
2020-03-02 18:38:51 +0100

    routes user & agent (GET/POST/PUT)

--- app/models/adModel.js
+++ app/models/adModel.js
@@ -1,7 +1,45 @@
 var mongoose = require('./bdd');
 
+var timeSlotSchema = mongoose.Schema({
+    booked: Boolean,
+    private: Boolean,
+    agent: {type: mongoose.Schema.Types.ObjectId, ref: 'agents'},
+    user: [{type: mongoose.Schema.Types.ObjectId, ref: 'users'}],
+    start: Date,
+    end: Date
+});
+
+var questionSchema = mongoose.Schema({
+    status: String,
+    question: String,
+    response: String,
+    user: {type: mongoose.Schema.Types.ObjectId, ref: 'users'}
+});
+
+var offerSchema = mongoose.Schema({
+    creationDate: Date,
+    user: {type: mongoose.Schema.Types.ObjectId, ref: 'users'},
+    singleBuyer: Boolean,
+    lastname1: String,
+    firstname1: String,
+    lastname2: String,
+    firstname2: String,
+    amount: Number,
+    loan: Boolean,
+    loanAmount: Number,
+    contributionAmount: Number,
+    monthlyPay: Number,
+    notary: Boolean,
+    notaryName: String,
+    notaryAddress: String,
+    notaryEmail: String,
+    validityPeriod: Number,
+    location: String,
+    message: String
+});
+
 var adSchema = mongoose.Schema({
-    creationDate: Date.now,
+    creationDate: Date,
     onlineDate: Date,
     color: String,
     onlineStatus: Boolean,
@@ -32,42 +70,6 @@ var adSchema = mongoose.Schema({
     offers: [offerSchema]
 });
 
-var timeSlotSchema = mongoose.Schema({
-    booked: Boolean,
-    agent: {type: Schema.Types.ObjectId, ref: 'agent'},
-    customer: [{type: Schema.Types.ObjectId, ref: 'customer'}],
-    start: Date,
-    end: Date
-});
-
-var questionSchema = mongoose.Schema({
-    status: String,
-    question: String,
-    response: String,
-    customer: {type: Schema.Types.ObjectId, ref: 'customer'}
-});
-
-var offerSchema = mongoose.Schema({
-    creationDate: Date.now,
-    customer: {type: Schema.Types.ObjectId, ref: 'customer'},
-    singleBuyer: Boolean,
-    lastname1: String,
-    firstname1: String,
-    lastname2: String,
-    firstname2: String,
-    loan: Boolean,
-    loanAmount: Number,
-    contributionAmount: Number,
-    monthlyPay: Number,
-    notary: Boolean,
-    notaryName: String,
-    notaryAddress: String,
-    notaryEmail: String,
-    validityPeriod: Number,
-    location: String,
-    message: String
-});
-
-var adModel = mongoose.model('ad', adSchema);
+var adModel = mongoose.model('ads', adSchema);
 
 module.exports = adModel;
