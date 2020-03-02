125e2c4e93249dc35147e595eb3fe05d1eef2dd9
Barthelemy Bouyer <b_bouyer@hotmail.fr>
2020-03-02 16:22:53 +0100

    calendar OK :)

--- fullcalendar/src/App.js
+++ fullcalendar/src/App.js
@@ -5,44 +5,26 @@ import timeGrid from '@fullcalendar/timegrid'
 import interaction from '@fullcalendar/interaction'
 
 import {RightOutlined, LeftOutlined, DownOutlined} from '@ant-design/icons'
-import {Menu, Dropdown, Modal, DatePicker, TimePicker, Select} from 'antd'
+import {Menu, Dropdown, Modal, DatePicker, TimePicker, Select, Button, Popconfirm, message} from 'antd'
 import locale from 'antd/es/date-picker/locale/fr_FR';
 import moment from 'moment'
 import 'moment/locale/fr';
 
-import './App.css' 
+import './Calendar.css'
 import 'antd/dist/antd.css';
+
+const ts = require("time-slots-generator");
 
 const { RangePicker } = TimePicker;
 const { Option } = Select;
 
-function App() {
+function Calendar() {
   
   const [myEvents, setMyEvents] = useState([]) 
   const [title, setTitle] = useState('')
   const [view, setView] = useState('Semaine')
 
-  const [appointmentModalVisible, setAppointmentModalVisible] = useState(false)
-  const [appointmentModalOkLoading, setAppointmentModalOkLoading] =useState(false)
-
-  const [newEventDate, setNewEventDate] = useState()
-  const [newEventHour1, setNewEventHour1] = useState()
-  const [newEventHour2, setNewEventHour2] = useState()
-  const [newEventProperty, setNewEventProperty] = useState()
-  const [newEventColor, setNewEventColor] = useState()
-
   var calendar = useRef(null)
-
-  const daysInFrench = {
-    '0' : 'Dim',
-    '1' : 'Lun',
-    '2' : 'Mar',
-    '3' : 'Mer',
-    '4' : 'Jeu',
-    '5' : 'Ven',
-    '6' : 'Sam',
-  }
-  const daysTranslate = (state) => daysInFrench[state];
 
   useEffect( () => { async function changeTitle() {
     setTitle(calendar.current.calendar.view.title)
@@ -50,7 +32,7 @@ function App() {
   changeTitle()
   }, []);
 
-  /* Choix de la vue : jour, semaine, mois */
+  /* View choice : day, week, month */
   const menu = (
     <Menu>
       <Menu.Item
@@ -86,46 +68,20 @@ function App() {
     </Menu>
   );
 
-  /*Modal*/
-  
-  const handleOk = () => {
-    setAppointmentModalOkLoading(true);
-    const year = newEventDate.slice(0,4)
-    let month = newEventDate.slice(5,7)
-    month = Number(month) -1
-    const day = newEventDate.slice(8,10)
-    const hour1 = newEventHour1.slice(0,2)
-    const min1 = newEventHour1.slice(3,5)
-    const hour2 = newEventHour2.slice(0,2)
-    const min2 = newEventHour2.slice(3,5)
+  /*----------------------------------------------- MODAL ---------------------------------------------------*/
+  const [appointmentModalVisible, setAppointmentModalVisible] = useState(false)
+  const [appointmentModalMode, setAppointmentModalMode] = useState(null)
+  const [appointmentModalOkLoading, setAppointmentModalOkLoading] =useState(false)
 
-    const date1 = new Date(year, month, day, hour1, min1)
-    const date2 = new Date(year, month, day, hour2, min2)
+  const [appointmentModalDate, setAppointmentModalDate] = useState()
+  const [appointmentModalHour1, setAppointmentModalHour1] = useState()
+  const [appointmentModalHour2, setAppointmentModalHour2] = useState()
+  const [appointmentModalProperty, setAppointmentModalProperty] = useState()
+  const [appointmentModalColor, setAppointmentModalColor] = useState()
+  const [appointmentModalColorInput, setAppointmentModalColorInput] = useState()
+  const [appointmentModalId, setAppointmentModalId] = useState(0)
 
-    let startDate 
-    let endDate
-    if (date1 < date2) {
-      startDate = date1
-      endDate = date2
-    } else {
-      startDate = date2
-      endDate = date1
-    }
-
-    //appel Base de données pour enregistrer les créneaux : simulation avec set time out
-    setTimeout(() => {
-      var eventsCopy = [...myEvents, {title: newEventProperty, start: startDate, end: endDate, color: newEventColor}];
-      setMyEvents(eventsCopy)
-
-      setAppointmentModalOkLoading(false)
-      setAppointmentModalVisible(false);
-    }, 2000)
-  };
-
-  const handleCancel = () => {
-    setAppointmentModalVisible(false);
-  }
-
+  /* Properties table (without DB)*/
   const properties = [
     "Appartement rue de Turbigo, Paris - 55m2 - 500 k€",
     "Maison rue Saint-Denis, Paris - 120m2 - 1500 k€",
@@ -141,6 +97,7 @@ function App() {
     </Option>
   ))
 
+  /* Color Init */
   const colors = [{
     hex: '#051F40', name: 'Bleu Marine'},
     {hex: '#1065CC', name: 'Bleu'},
@@ -149,7 +106,6 @@ function App() {
     {hex: '#f9ca24', name: 'Jaune'},
     {hex: '#95afc0', name: 'Gris'
   }]
-
 
   const colorOptions = colors.map( e => (
     <Option
@@ -168,12 +124,152 @@ function App() {
     </Option>
   ))
 
+
+  /* Modal Footer*/
+
+  /* TimeSlot: Convert time to hour and minutes */
+  function minToHandM(n) {
+    var num = n;
+    var hours = (num / 60);
+    var rhours = Math.floor(hours);
+    var minutes = (hours - rhours) * 60;
+    var rminutes = Math.round(minutes);
+    return {hour: rhours, minute: rminutes};
+    }
+
+  /* HANDLING BUTTONS */
+  const handleOk = () => {
+    setAppointmentModalOkLoading(true);
+    const year = appointmentModalDate.slice(0,4)
+    let month = Number(appointmentModalDate.slice(5,7))-1
+    const day = appointmentModalDate.slice(8,10)
+    const hour1 = appointmentModalHour1.slice(0,2)
+    const min1 = appointmentModalHour1.slice(3,5)
+    const hour2 = appointmentModalHour2.slice(0,2)
+    const min2 = appointmentModalHour2.slice(3,5)
+
+    const date1 = new Date(year, month, day, hour1, min1)
+    const date2 = new Date(year, month, day, hour2, min2)
+
+      /* Compare dates because we do not get them ordered */
+    let startTime
+    let endTime
+    if (date1 < date2) {
+      startTime = Number(hour1)*60+Number(min1)
+      endTime = Number(hour2)*60+Number(min2)
+    } else {
+      startTime = Number(hour2)*60+Number(min2)
+      endTime = Number(hour1)*60+Number(min1)
+    }
+      /* Warning: timeslots does not include bounds (so first slot is starTime minus the interval) */
+    var interval = 30;
+    var slots = ts.getTimeSlots([[0, startTime-interval], [endTime, 1450]], false, "half")
+    slots = slots.map( (e,i) => {
+      var start = new Date(year, month, day, minToHandM(e).hour, minToHandM(e).minute)
+      return {
+        title: appointmentModalProperty,
+        start: start,
+        end: moment(start).add(interval, 'm').toDate(),
+        color: appointmentModalColor,
+        id: Math.floor(Math.random()*100000)
+      }
+    })
+
+    //appel Base de données pour enregistrer les créneaux : simulation avec set time out
+    setTimeout(() => {
+      var eventsCopy=[...myEvents];
+      if (appointmentModalMode === "edit") {
+        eventsCopy = eventsCopy.filter( e => {
+          console.log(e.id)
+          console.log(appointmentModalId)
+          return (e.id != appointmentModalId) })
+      }
+      eventsCopy = eventsCopy.concat(slots)
+      setMyEvents(eventsCopy)
+
+      setAppointmentModalOkLoading(false);
+      setAppointmentModalVisible(false);
+      setAppointmentModalDate(null);
+      setAppointmentModalHour1(null);
+      setAppointmentModalHour2(null);
+      setAppointmentModalProperty(null);
+      setAppointmentModalColor(null);
+      setAppointmentModalId(null);
+      setAppointmentModalMode(null);
+    }, 2000)
+  };
+
+  const handleCancel = () => {
+    setAppointmentModalVisible(false);
+    setAppointmentModalDate(null);
+    setAppointmentModalHour1(null);
+    setAppointmentModalHour2(null);
+    setAppointmentModalProperty(null);
+    setAppointmentModalColor(null);
+    setAppointmentModalId(null);
+    setAppointmentModalMode(null);
+  }
+
+  function confirm(e) {
+    setMyEvents(myEvents.filter( e => e.id != appointmentModalId))
+    message.success('Créneau supprimé');
+    setAppointmentModalVisible(false);
+    setAppointmentModalDate(null);
+    setAppointmentModalHour1(null);
+    setAppointmentModalHour2(null);
+    setAppointmentModalProperty(null);
+    setAppointmentModalColor(null);
+    setAppointmentModalId(null);
+    setAppointmentModalMode(null);
+  }
+
+  const modalFooter =
+    <div className="modal-footer">
+
+      {appointmentModalMode === "edit" ? 
+        <Popconfirm
+          title="Confirmer la suppression du créneau ?"
+          onConfirm={confirm}
+          okText="Oui"
+          cancelText="Non"
+          placement="bottomLeft"
+        >
+          <Button className="button-delete">
+            Supprimer
+          </Button>
+        </Popconfirm>
+        : <div></div>
+      }
+  
+      <div>
+        <Button className="button-cancel" onClick={handleCancel}>
+        Annuler
+        </Button>
+        <Button className="button-validate" loading={appointmentModalOkLoading} onClick={handleOk}>
+          Valider
+        </Button>
+      </div>
+    </div>
+
+  /* Column Header translation */
+  const daysInFrench = {
+    '0' : 'Dim',
+    '1' : 'Lun',
+    '2' : 'Mar',
+    '3' : 'Mer',
+    '4' : 'Jeu',
+    '5' : 'Ven',
+    '6' : 'Sam',
+  }
+  const daysTranslate = (state) => daysInFrench[state];
+
+
   return (
     <div>
-      <div className="header" style={styles.header}>
-        <div className="headerNavLeft" style={styles.headerNavLeft}>
+      <div className="calendar-header">
+        <div className="calendar-headerNavLeft">
           <LeftOutlined
-            style={styles.chevronIcon}
+            className="calendar-headerNavLeft-chevronIcon"
             onClick={ () => {
               const calendarApi = calendar.current.getApi()
               calendarApi.prev();
@@ -181,12 +277,12 @@ function App() {
             }}
           />
 
-          <div className="dateTitle" style={styles.dateTitle}>
+          <div className="calendar-headerNavLeft-dateTitle">
             {title}
           </div>
 
           <RightOutlined
-            style={styles.chevronIcon}
+            className="calendar-headerNavLeft-chevronIcon"
             onClick={ () => {
               const calendarApi = calendar.current.getApi()
               calendarApi.next();
@@ -195,27 +291,39 @@ function App() {
           />
         </div>
 
-        <Dropdown overlay={menu} trigger={['click']}>
-          <a className="ant-dropdown-link" style={styles.headerNavRight} onClick={e => e.preventDefault()}>
+        <Dropdown className="calendar-headerNavRight" overlay={menu} trigger={['click']}>
+          <a className="ant-dropdown-link" onClick={e => e.preventDefault()}>
             {view} <DownOutlined />
           </a>
         </Dropdown>
-      
       </div>
+
       <FullCalendar
+        /* Global Settings */
         ref={calendar}
-        defaultView="timeGridWeek"
         plugins={[ dayGridPlugin, timeGrid, interaction ]}
+        defaultView="timeGridWeek"
         locale= 'fr'
-        firstDay= {1}
-        hiddenDays={[0]}
-        
         header={{
           left: '',
           center: '',
           right: ''
           }}
+        contentHeight= "auto"
 
+        /* Events */
+        events={myEvents}
+        eventColor={'#052040'}
+
+        /* Time Settings */
+        firstDay= {1}
+        hiddenDays={[0]}
+        allDaySlot={false}
+        minTime={'08:00'}
+        maxTime={'20:00'}
+        defaultTimedEventDuration={'00:30'}
+
+        /* Column headers */
         columnHeaderHtml={ (date) => {
           if (view==='Semaine') {
             return (
@@ -241,62 +349,58 @@ function App() {
         }
         }}
         
-        allDaySlot={false}
-        
+        /*Manage clicks on elements*/
         selectable= {true}
         navLinks= {true}
         navLinkDayClick="timeGridDay"
         select={(info) => {
-          setNewEventDate(info.start.toISOString().slice(0,10))
-          setNewEventHour1(info.start.toTimeString().slice(0,5))
-          setNewEventHour2(info.end.toTimeString().slice(0,5))
+          setAppointmentModalDate(info.start.toISOString().slice(0,10))
+          setAppointmentModalHour1(info.start.toTimeString().slice(0,5))
+          setAppointmentModalHour2(info.end.toTimeString().slice(0,5))
           setAppointmentModalVisible(true);
           }}
-        events={myEvents}
-        minTime={'08:00'}
-        maxTime={'20:00'}
-        defaultTimedEventDuration={'00:30'}
-        eventColor={'#052040'}
-        
-        contentHeight= "auto"
-
-        // businessHours={{
-        //   startTime: '10:00',
-        //   endTime: '18:00'
-        // }}  
-        
+        eventClick= { (info) => {
+          setAppointmentModalDate(info.event.start.toISOString().slice(0,10))
+          setAppointmentModalHour1(info.event.start.toTimeString().slice(0,5))
+          setAppointmentModalHour2(info.event.end.toTimeString().slice(0,5))
+          setAppointmentModalProperty(info.event.title)
+          setAppointmentModalColor(info.event.backgroundColor)
+          setAppointmentModalColorInput(<span className="dot" style={{
+            backgroundColor: info.event.backgroundColor,
+            height: '15px',
+            width: '15px',
+            borderRadius: '50%',
+            display: 'inline-block',
+            marginRight: '1em'
+          }}></span>)
+          setAppointmentModalId(info.event.id)
+          setAppointmentModalMode('edit')
+          setAppointmentModalVisible(true)
+        }}    
       />
 
       <Modal
-        title="Nouveau Rendez-Vous"
+        title={appointmentModalMode === "edit" ? `Modifier: ${appointmentModalProperty}` : "Nouveau Rendez-Vous"}
         visible={appointmentModalVisible}
-        okText="Valider RDV"
-        onOk={ () => handleOk()}
-        okButtonProps= {{
-          style:{
-            backgroundColor:"#052040",
-            borderColor: "#052040",
-          }
-        }}
-        confirmLoading={appointmentModalOkLoading}
-        cancelText= "Annuler"
-        onCancel={ () => handleCancel()}
+        footer= {modalFooter}
         destroyOnClose= {true}
-        width= "70%"
+        width= "50%"
+        closable={false}
+        maskClosable={true}
       >
-        <div className='inputModal' style={styles.inputModal}>
-          <p className="inputLabelModal" style={styles.inputLabelModal}>Date du RDV</p>
+        <div className='input-modal'>
+          <p className="input-modal-label">Date du RDV</p>
           <DatePicker
             locale={locale}
-            defaultValue={moment(newEventDate, 'YYYY-MM-DD')}
+            defaultValue={moment(appointmentModalDate, 'YYYY-MM-DD')}
             onChange= {(date, dateString) => {
-              setNewEventDate(dateString)
+              setAppointmentModalDate(dateString)
             }}
           />
         </div>
 
-        <div className='inputModal' style={styles.inputModal}>
-          <p className="inputLabelModal" style={styles.inputLabelModal}>Horaires du RDV</p>
+        <div className='input-modal'>
+          <p className="input-modal-label">Horaires du RDV</p>
           <RangePicker
             locale={locale}
             format= 'HH:mm'
@@ -304,22 +408,24 @@ function App() {
             disabledHours={() => [0, 1, 2, 3, 4, 5, 6, 7, 20, 21, 22, 23]}
             hideDisabledOptions={true}
             placeholder={["Début", "Fin"]}
-            defaultValue={[moment(newEventHour1, 'HH:mm'), moment(newEventHour2, 'HH:mm')]}
+            defaultValue={[moment(appointmentModalHour1, 'HH:mm'), moment(appointmentModalHour2, 'HH:mm')]}
             onChange= { (time, timeSring) => {
-              setNewEventHour1(timeSring[0]);
-              setNewEventHour2(timeSring[1]);
+              setAppointmentModalHour1(timeSring[0]);
+              setAppointmentModalHour2(timeSring[1]);
             }}
           />
         </div>
 
-        <div className='inputModal' style={styles.inputModal}>
-          <p className="inputLabelModal" style={styles.inputLabelModal}>Choix du bien</p>
+        {appointmentModalMode !== "edit" && 
+        <div className='input-modal'>
+          <p className="input-modal-label">Choix du bien</p>
           <Select
             showSearch
-            style={{ width: 500 }}
+            style={{ width: '80%' }}
             placeholder="Bien à faire visiter"
             optionFilterProp="children"
-            onChange={value => setNewEventProperty(value)}
+            defaultValue= {appointmentModalProperty}
+            onChange={value => setAppointmentModalProperty(value)}
             filterOption={(input, option) =>
               option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
             }
@@ -327,16 +433,18 @@ function App() {
             {propertiesOptions}
           </Select>
         </div>
+        }
 
-        <div className='inputModal' style={styles.inputModal}>
-          <p className="inputLabelModal" style={styles.inputLabelModal}>Couleur dans l'agenda</p>
+        <div className='input-modal'>
+          <p className="input-modal-label">Couleur dans l'agenda</p>
           <Select
             showSearch
             style={{ width: 200 }}
             placeholder="Couleur"
             optionFilterProp="children"
+            defaultValue= {appointmentModalColorInput}
             onChange={ (value, option) => {
-              setNewEventColor(option.key)}}
+              setAppointmentModalColor(option.key)}}
             filterOption={(input, option) =>
               option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
             }
@@ -345,55 +453,10 @@ function App() {
           </Select>
         </div>
       </Modal>
+
     </div>
     
   );
 }
 
-export default App;
-
-const styles = {
-  header: {
-    display: "flex",
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    marginTop: "2em",
-    marginBottom: "1em"
-  },
-
-    headerNavLeft: {
-      width: "33%",
-      display: "flex",
-      flexDirection: "row",
-      alignItems: "center",
-      justifyContent: "space-around",
-      marginLeft: 15,
-      
-    },
-      chevronIcon: {
-        color: "#6F6E6E",
-        fontSize: 20,
-      },
-      dateTitle:{
-        margin:0,
-        textAlign: "center",
-        color: "#052040"
-      },
-    
-    headerNavRight: {
-      marginRight: 15,
-      fontSize: 14,
-      color: "#052040",
-      textDecoration: "underline"
-    },
-
-  inputModal: {
-    display: "flex",
-    alignItems: "center",
-    marginBottom: "1em"
-  },
-    inputLabelModal: {
-      margin: "0 1em"
-    }
-}
+export default Calendar;
